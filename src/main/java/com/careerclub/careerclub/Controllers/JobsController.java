d1ce8a5502f6a885b7523f54a6f99a4795828339
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-10-17 14:25:18 +0300

    Added an error exception

---
+++ src/main/java/com/careerclub/careerclub/Controllers/JobsController.java
@@ -0,0 +1,2 @@
+package com.careerclub.careerclub.Controllers;public class JobsController {
+}
--- src/main/java/com/careerclub/careerclub/Service/JobsService.java
+++ src/main/java/com/careerclub/careerclub/Service/JobsService.java
@@ -1,5 +1,6 @@
 package com.careerclub.careerclub.Service;
 
+import com.careerclub.careerclub.Advice.RecordNotFoundException;
 import com.careerclub.careerclub.DTOs.JobPostingRequest;
 import com.careerclub.careerclub.Entities.Job;
 import com.careerclub.careerclub.Repositories.JobRepository;
@@ -32,7 +33,7 @@ public class JobsService {
         return newJob;
     }
 
-    public Optional<Job> updateJob(Long id, Job job){
+    public Optional<Job> updateJob(Long id, Job job) {
         var jobToUpdate = jobRepository.findById(id);
 
         jobToUpdate.ifPresentOrElse(job1 -> {
@@ -40,10 +41,27 @@ public class JobsService {
             job1.setDeadline(job.getDeadline());
             job1.setTitle(job.getTitle());
             job1.setJobType(job.getJobType());
+            job1.setDescription(job.getDescription());
             jobRepository.save(job1);
-        },()->{
-
+        }, () -> {
+            throw new RecordNotFoundException("Job doesn't Exist");
         });
         return jobToUpdate;
+    }
+
+    public Optional<Job> getJobById(Long id){
+        var job = jobRepository.findById(id);
+        return job;
+    }
+
+    public String jobToDelete(Long id){
+        var jobToBeDeleted = jobRepository.findById(id);
+        jobToBeDeleted.ifPresentOrElse(job -> {
+            jobRepository.delete(job);
+        }, ()->{
+            throw new RecordNotFoundException("Job doesn't exist");
+        });
+        return "Job deleted succesfully";
+    }
 }
-}
+
