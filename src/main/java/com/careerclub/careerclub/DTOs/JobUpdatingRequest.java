1175fa29065aabf01b4579c50b752a92a702adc3
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-10-17 15:11:12 +0300

    Job updating

--- src/main/java/com/careerclub/careerclub/DTOs/JobUpdatingRequest.java
+++ src/main/java/com/careerclub/careerclub/DTOs/JobUpdatingRequest.java
@@ -1,2 +1,49 @@
-package com.careerclub.careerclub.DTOs;public class JobUpdatingRequest {
+package com.careerclub.careerclub.DTOs;
+
+public class JobUpdatingRequest {
+    private String description;
+    private String title;
+    private String qualification;
+    private String deadline;
+    private String jobType;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getQualification() {
+        return qualification;
+    }
+
+    public void setQualification(String qualification) {
+        this.qualification = qualification;
+    }
+
+    public String getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(String deadline) {
+        this.deadline = deadline;
+    }
+
+    public String getJobType() {
+        return jobType;
+    }
+
+    public void setJobType(String jobType) {
+        this.jobType = jobType;
+    }
 }
