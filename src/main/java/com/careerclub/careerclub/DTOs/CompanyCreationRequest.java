bd20965b568b1a5c06277195efec1388ce8916f6
Martin Mwangi <mwangi.martin24@gmail.com>
2022-10-17 14:50:58 +0300

    Create company entity

---
+++ src/main/java/com/careerclub/careerclub/DTOs/CompanyCreationRequest.java
@@ -0,0 +1,40 @@
+package com.careerclub.careerclub.DTOs;
+
+public class CompanyCreationRequest {
+    private Long companyId;
+    private String name;
+    private String description;
+    private String link;
+
+    public Long getCompanyId(){
+        return companyId;
+    }
+
+     public void setCompanyId(Long companyId){
+        this.companyId = companyId;
+     }
+
+     public String getName(){
+        return name;
+     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+}
