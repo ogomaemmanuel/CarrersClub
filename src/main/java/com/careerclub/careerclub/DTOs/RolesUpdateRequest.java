3b83afb050fbe44a53ec254a8de4a53eac14021b
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-10-17 11:21:05 +0300

    Added validation to the roles dto's

--- src/main/java/com/careerclub/careerclub/DTOs/RolesUpdateRequest.java
+++ src/main/java/com/careerclub/careerclub/DTOs/RolesUpdateRequest.java
@@ -1,6 +1,10 @@
 package com.careerclub.careerclub.DTOs;
 
+import javax.validation.constraints.NotBlank;
+
 public class RolesUpdateRequest {
+
+    @NotBlank(message = "Name is Required")
     private String name;
 
     public String getName() {
