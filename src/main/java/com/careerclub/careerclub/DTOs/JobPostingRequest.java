19480dd9a28a210a13201090c3bdde18ce4d54f4
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-10-17 11:58:54 +0300

    Job posting request

---
+++ src/main/java/com/careerclub/careerclub/DTOs/JobPostingRequest.java
@@ -0,0 +1,54 @@
+package com.careerclub.careerclub.DTOs;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+public class JobPostingRequest {
+
+    private String description;
+    private String title;
+    private String qualification;
+    private String deadline;
+    private String jobType;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getQualification() {
+        return qualification;
+    }
+
+    public void setQualification(String qualification) {
+        this.qualification = qualification;
+    }
+
+    public String getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(String deadline) {
+        this.deadline = deadline;
+    }
+
+    public String getJobType() {
+        return jobType;
+    }
+
+    public void setJobType(String jobType) {
+        this.jobType = jobType;
+    }
+}
