b953e9386baf2e3be77023519e0c7881d19cc26d
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-10-17 15:10:35 +0300

    Added a company repository

--- src/main/java/com/careerclub/careerclub/Repositories/CompanyRepository.java
+++ src/main/java/com/careerclub/careerclub/Repositories/CompanyRepository.java
@@ -1,2 +1,9 @@
-package com.careerclub.careerclub.Repositories;public class CompanyRepository {
+package com.careerclub.careerclub.Repositories;
+
+import com.careerclub.careerclub.Entities.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<Company, Long> {
 }
