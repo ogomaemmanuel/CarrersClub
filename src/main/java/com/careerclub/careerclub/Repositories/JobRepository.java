34454aa27aeb6632cc4cd56025baa97be9339768
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-10-17 11:59:24 +0300

    Job repository

--- src/main/java/com/careerclub/careerclub/Repositories/JobRepository.java
+++ src/main/java/com/careerclub/careerclub/Repositories/JobRepository.java
@@ -1,2 +1,9 @@
-package com.careerclub.careerclub.Repositories;public interface JobRepository {
+package com.careerclub.careerclub.Repositories;
+
+import com.careerclub.careerclub.Entities.Job;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JobRepository extends JpaRepository<Job, Long> {
 }
