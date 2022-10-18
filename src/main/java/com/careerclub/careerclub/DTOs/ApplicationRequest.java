b717c4f839d54d8895e1b0cc5ec238371f8fad71
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-10-18 12:26:39 +0300

    Applications creation dto

--- src/main/java/com/careerclub/careerclub/DTOs/ApplicationRequest.java
+++ src/main/java/com/careerclub/careerclub/DTOs/ApplicationRequest.java
@@ -1,2 +1,41 @@
-package com.careerclub.careerclub.DTOs;public class ApplicationRequest {
+package com.careerclub.careerclub.DTOs;
+
+import com.careerclub.careerclub.Entities.Job;
+import com.careerclub.careerclub.Entities.User;
+
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+
+public class ApplicationRequest {
+    private Long jobId;
+
+    private Long userId;
+
+    @Lob
+    private byte[] cv;
+
+    public Long getJobId() {
+        return jobId;
+    }
+
+    public void setJobId(Long jobId) {
+        this.jobId = jobId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public byte[] getCv() {
+        return cv;
+    }
+
+    public void setCv(byte[] cv) {
+        this.cv = cv;
+    }
 }
