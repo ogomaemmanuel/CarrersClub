fb2c5a26da71846a46c37dff7cc75266de597e32
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-10-18 08:06:29 +0300

    Added a custom query to retrieve otp code based on the user

--- src/main/java/com/careerclub/careerclub/Repositories/CodeRepository.java
+++ src/main/java/com/careerclub/careerclub/Repositories/CodeRepository.java
@@ -1,9 +1,11 @@
 package com.careerclub.careerclub.Repositories;
 
 import com.careerclub.careerclub.Entities.Code;
+import com.careerclub.careerclub.Entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CodeRepository extends JpaRepository<Code,Long> {
+    Code findByUser(User user);
 }
