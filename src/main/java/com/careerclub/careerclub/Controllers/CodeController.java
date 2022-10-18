4a2b56a05368acbbe7ac7ba3beeb7dafed36f717
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-10-18 09:28:46 +0300

    Added an endpoint to retrieve all codes

--- src/main/java/com/careerclub/careerclub/Controllers/CodeController.java
+++ src/main/java/com/careerclub/careerclub/Controllers/CodeController.java
@@ -1,4 +1,27 @@
 package com.careerclub.careerclub.Controllers;
 
+import com.careerclub.careerclub.Entities.Code;
+import com.careerclub.careerclub.Service.CodeService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/code")
 public class CodeController {
+    private final CodeService codeService;
+
+    public CodeController(CodeService codeService) {
+        this.codeService = codeService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Code>> getAllCodes(){
+        var codes = codeService.getAllCodes();
+        return ResponseEntity.ok(codes);
+    }
+
 }
