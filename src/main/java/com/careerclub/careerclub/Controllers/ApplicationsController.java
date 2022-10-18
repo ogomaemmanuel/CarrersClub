33f804094be13b7b1f0cbc303db890b074515e38
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-10-18 12:25:15 +0300

    Making an application controller

--- src/main/java/com/careerclub/careerclub/Controllers/ApplicationsController.java
+++ src/main/java/com/careerclub/careerclub/Controllers/ApplicationsController.java
@@ -1,4 +1,36 @@
 package com.careerclub.careerclub.Controllers;
 
-public class Applications {
+import com.careerclub.careerclub.DTOs.ApplicationRequest;
+import com.careerclub.careerclub.Entities.Application;
+import com.careerclub.careerclub.Service.ApplicationService;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/applications")
+public class ApplicationsController {
+    private final ApplicationService applicationService;
+
+    public ApplicationsController(ApplicationService applicationService) {
+        this.applicationService = applicationService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Application>> getAllApplications(){
+        return ResponseEntity.ok(applicationService.getAllApplications());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Application> getApplicationById(@PathVariable Long id){
+        return ResponseEntity.of(applicationService.getApplicationById(id));
+    }
+    @PostMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public ResponseEntity<Application> makeApplication(ApplicationRequest applicationRequest){
+        return ResponseEntity.ok(applicationService.makeAnApplication(applicationRequest));
+    }
 }
