61eabf4e3b9b68a445966a407419bbfbf9e76800
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-10-19 08:35:19 +0300

    Added DbSeeders for job and company

--- src/main/java/com/careerclub/careerclub/Seeders/DbSeeders.java
+++ src/main/java/com/careerclub/careerclub/Seeders/DbSeeders.java
@@ -1,7 +1,11 @@
 package com.careerclub.careerclub.Seeders;
 
+import com.careerclub.careerclub.Entities.Company;
+import com.careerclub.careerclub.Entities.Job;
 import com.careerclub.careerclub.Entities.Roles;
 import com.careerclub.careerclub.Entities.User;
+import com.careerclub.careerclub.Repositories.CompanyRepository;
+import com.careerclub.careerclub.Repositories.JobRepository;
 import com.careerclub.careerclub.Repositories.RolesRepository;
 import com.careerclub.careerclub.Repositories.UserRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -9,7 +13,6 @@ import org.springframework.stereotype.Component;
 
 import javax.transaction.Transactional;
 import java.util.ArrayList;
-import java.util.HashSet;
 
 @Component
 @Transactional
@@ -18,16 +21,22 @@ public class DbSeeders implements CommandLineRunner {
 
     private final UserRepository userRepository;
     private final RolesRepository rolesRepository;
+    private final JobRepository jobRepository;
+    private final CompanyRepository companyRepository;
 
-    public DbSeeders(UserRepository userRepository, RolesRepository rolesRepository) {
+    public DbSeeders(UserRepository userRepository, RolesRepository rolesRepository, JobRepository jobRepository, CompanyRepository companyRepository) {
         this.userRepository = userRepository;
         this.rolesRepository = rolesRepository;
+        this.jobRepository = jobRepository;
+        this.companyRepository = companyRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
         var userCount = userRepository.count();
         var roleCount = rolesRepository.count();
+        var jobCount = jobRepository.count();
+        var companyCount = companyRepository.count();
         if(roleCount==0){
             var rolesToAdd = new String[]{"admin", "member","hr"};
             for(String s:rolesToAdd){
@@ -45,6 +54,24 @@ public class DbSeeders implements CommandLineRunner {
             user.setPassword("Testing");
             user.setRoles(roles);
             userRepository.save(user);
+        }
+        if(companyCount==0){
+            var company = new Company();
+            company.setName("TestingPLC");
+            company.setDescription("A testing company");
+            company.setLink("https://crabs-eugene.netlify.app/");
+            companyRepository.save(company);
+        }
+        if(jobCount==0){
+            var testCompany = companyRepository.findById(1L).get();
+            var job = new Job();
+            job.setTitle("DevOps Engineer");
+            job.setDescription("Development and Deploying");
+            job.setJobType("Full-Time");
+            job.setDeadline("08/12/2022");
+            job.setQualification("Docker & Kubernettes");
+            job.setCompany(testCompany);
+            jobRepository.save(job);
         }
     }
 }
