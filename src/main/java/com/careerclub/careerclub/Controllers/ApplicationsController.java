bb12377bc3c2e928a87117348d2b289b1fcd7330
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-10-19 10:05:26 +0300

    Changed some stuff

--- src/main/java/com/careerclub/careerclub/Controllers/ApplicationsController.java
+++ src/main/java/com/careerclub/careerclub/Controllers/ApplicationsController.java
@@ -3,6 +3,7 @@ package com.careerclub.careerclub.Controllers;
 import com.careerclub.careerclub.DTOs.ApplicationRequest;
 import com.careerclub.careerclub.Entities.Application;
 import com.careerclub.careerclub.Service.ApplicationService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -29,8 +30,13 @@ public class ApplicationsController {
     public ResponseEntity<Application> getApplicationById(@PathVariable Long id){
         return ResponseEntity.of(applicationService.getApplicationById(id));
     }
-    @PostMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    public ResponseEntity<Application> makeApplication(ApplicationRequest applicationRequest){
-        return ResponseEntity.ok(applicationService.makeAnApplication(applicationRequest));
-    }
+
+////    @PostMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+////    public ResponseEntity<Application> makeApplication(ApplicationRequest applicationRequest){
+////        return ResponseEntity.ok(applicationService.makeAnApplication(applicationRequest));
+////    }
+//    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+//    public ResponseEntity<Application> saveApplication(@RequestParam("file") MultipartFile file){
+//        return new ResponseEntity<>(applicationService.(file), HttpStatus.OK)
+//    }
 }
