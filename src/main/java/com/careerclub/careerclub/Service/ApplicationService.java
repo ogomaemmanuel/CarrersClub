5f8e16fc7431559885ce09b050186a6a34af2671
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-10-19 10:45:05 +0300

    Made some changes

--- src/main/java/com/careerclub/careerclub/Service/ApplicationService.java
+++ src/main/java/com/careerclub/careerclub/Service/ApplicationService.java
@@ -16,7 +16,6 @@ import com.careerclub.careerclub.Utils.FileUpload;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-
 import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
@@ -46,13 +45,6 @@ public class ApplicationService {
         return application;
     }
 
-
-    public void upload(String path, String fileName, Optional<Map<String, String>> optionalMetaData, InputStream inputStream){
-        ObjectMetadata objectMetadata = new ObjectMetadata();
-        optionalMetaData.ifPresent(map ->{
-            if(!map.isEmpty()){
-                map.forEach(objectMetadata::addUserMetadata);
-            }
     public Application makeAnApplication(MultipartFile cv, Long jobId, Long userId){
         Application newApplication = new Application();
         var user = userRepository.findById(userId);
@@ -71,26 +63,13 @@ public class ApplicationService {
             });
         }, () -> {
             throw new RecordNotFoundException("Company doesn't exist");
-
         });
-        try{
-            amazonS3.putObject(path, fileName, inputStream, objectMetadata);
-        }catch (AmazonServiceException e){
-            throw new IllegalStateException("Failed to upload the file", e);
-        }
+        return newApplication;
     }
 
-    public byte[] download(String path, String key){
-        try{
-            S3Object object = amazonS3.getObject(path, key);
-            S3ObjectInputStream objectContent = object.getObjectContent();
-            return IOUtils.toByteArray(objectContent);
-        }catch(AmazonServiceException | IOException e){
-            throw new IllegalStateException("Failed to download the file", e);
-        }
-    }
-
-
+    public InputStream getCv(CvDownloadRequest downloadRequest) {
+        S3Object s3Object= amazonS3.getObject(downloadRequest.getCvPath(),downloadRequest.getCvFileName());
+        return s3Object.getObjectContent();
     }
 
 
