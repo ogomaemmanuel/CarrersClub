84f018be7eed94f544b10b8fb51c152e36750c9f
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-10-19 08:34:44 +0300

    Configured an amazon bucket

--- src/main/java/com/careerclub/careerclub/Config/BucketName.java
+++ src/main/java/com/careerclub/careerclub/Config/BucketName.java
@@ -1,2 +1,14 @@
-package com.careerclub.careerclub.Config;public class BucketName {
+package com.careerclub.careerclub.Config;
+
+public enum BucketName {
+    CV("aws-tutorial-ogoma");
+    private final String bucketName;
+
+    BucketName(String bucketName) {
+        this.bucketName = bucketName;
+    }
+
+    public String getBucketName() {
+        return bucketName;
+    }
 }
