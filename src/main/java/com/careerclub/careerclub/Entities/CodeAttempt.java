f38092c243ba5dd2de0a17f55db2e742ee23c01a
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-10-19 08:36:36 +0300

    Added a code attempt entity to track code attempt when verifing the code

--- src/main/java/com/careerclub/careerclub/Entities/CodeAttempt.java
+++ src/main/java/com/careerclub/careerclub/Entities/CodeAttempt.java
@@ -1,2 +1,45 @@
-package com.careerclub.careerclub.Entities;public class CodeAttempt {
+package com.careerclub.careerclub.Entities;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "code_attempts")
+public class CodeAttempt {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
