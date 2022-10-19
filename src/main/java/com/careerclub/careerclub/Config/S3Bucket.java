1f43e4dd5d383627de37d17748671bb533f105f1
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-10-19 10:04:50 +0300

    Setting up the s3 bucket

--- src/main/java/com/careerclub/careerclub/Config/S3Bucket.java
+++ src/main/java/com/careerclub/careerclub/Config/S3Bucket.java
@@ -1,2 +1,10 @@
-package com.careerclub.careerclub.Config;public class S3Bucket {
+package com.careerclub.careerclub.Config;
+
+public enum S3Bucket {
+    TODO_IMAGE("aws-tutorial-ogoma");
+    private final String bucketName;
+
+    S3Bucket(String bucketName) {
+        this.bucketName = bucketName;
+    }
 }
