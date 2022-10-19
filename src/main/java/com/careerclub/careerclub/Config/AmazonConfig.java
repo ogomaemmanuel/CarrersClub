372df8b4506e897661aadb877b6aab7dd450f23d
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-10-19 10:04:17 +0300

    Setting up amazon credentials

--- src/main/java/com/careerclub/careerclub/Config/AmazonConfig.java
+++ src/main/java/com/careerclub/careerclub/Config/AmazonConfig.java
@@ -1,2 +1,27 @@
-package com.careerclub.careerclub.Config;public class AmazonConfig {
+package com.careerclub.careerclub.Config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AmazonConfig {
+    @Value("{$AWS_ACCESS_KEY_ID}")
+    private String accessKey;
+    @Value("{$AWS_SECRET_ACCESS_KEY}")
+    private String secretKey;
+    @Value("{$AWS_REGION}")
+    private String region;
+    @Bean
+    public AmazonS3 s3(){
+        AWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+        return AmazonS3ClientBuilder.standard().
+                withRegion(region).withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
+    }
 }
