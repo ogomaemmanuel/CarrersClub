65841fbe9e13d078380acca241e821ca948dc27c
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-10-20 05:51:52 +0300

    Created an error message converter to filter error messages thrown by validator

--- src/main/java/com/careerclub/careerclub/Utils/ErrorConverter.java
+++ src/main/java/com/careerclub/careerclub/Utils/ErrorConverter.java
@@ -1,2 +1,33 @@
-package com.careerclub.careerclub.Utils;public class ErrorConverter {
+package com.careerclub.careerclub.Utils;
+
+import org.springframework.validation.Errors;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ErrorConverter {
+    public static Map<String, ArrayList<String>> convert(Errors errors){
+        Map<String, ArrayList<String>> apiErrors = new HashMap<>();
+        ArrayList<String> globalErrorList = new ArrayList<>();
+        errors.getFieldErrors().forEach(fieldError->{
+            String field=fieldError.getField();
+            String errorMessage=fieldError.getDefaultMessage();
+            if(apiErrors.containsKey(field)){
+                ArrayList<String> fieldErrors= apiErrors.get(field);
+                fieldErrors.add(errorMessage);
+                apiErrors.put(field,fieldErrors);
+            }
+            else {
+                ArrayList<String> errorList = new ArrayList<>();
+                errorList.add(errorMessage);
+                apiErrors.put(field,errorList);
+            }
+        });
+        errors.getGlobalErrors().forEach(globalError->{
+            globalErrorList.add(globalError.getDefaultMessage());
+            apiErrors.put("globalError",globalErrorList);
+        });
+        return apiErrors;
+    }
 }
