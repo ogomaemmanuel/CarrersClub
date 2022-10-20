21df15c5b05ac6453786bad15a46d5544cf85a03
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-10-20 21:23:13 +0300

    Added pageables

--- src/main/java/com/careerclub/careerclub/Controllers/JobsController.java
+++ src/main/java/com/careerclub/careerclub/Controllers/JobsController.java
@@ -5,6 +5,8 @@ import com.careerclub.careerclub.DTOs.JobUpdatingRequest;
 import com.careerclub.careerclub.Entities.Job;
 import com.careerclub.careerclub.Service.JobsService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,8 +22,8 @@ public class JobsController {
     }
 
     @GetMapping
-    public ResponseEntity<List<Job>> getAllJobs(){
-        return  ResponseEntity.ok(jobsService.getAllJobs());
+    public ResponseEntity<Page<Job>> getAllJobs(Pageable pageable){
+        return  ResponseEntity.ok(jobsService.getAllJobs(pageable));
     }
     @GetMapping("/{id}")
     public ResponseEntity<Job> getJobById(@PathVariable Long id){
