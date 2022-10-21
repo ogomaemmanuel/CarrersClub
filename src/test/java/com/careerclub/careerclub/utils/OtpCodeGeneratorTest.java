e4cc15791972c146ddc005d0e20d419e895c67d4
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-10-21 07:17:44 +0300

    Testing code generation util

--- src/test/java/com/careerclub/careerclub/utils/OtpCodeGeneratorTest.java
+++ src/test/java/com/careerclub/careerclub/utils/OtpCodeGeneratorTest.java
@@ -1,2 +1,17 @@
-package com.careerclub.careerclub.utils;public class OtpCodeGeneratorTest {
+package com.careerclub.careerclub.utils;
+
+import com.careerclub.careerclub.Utils.OtpCodeGenerator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class OtpCodeGeneratorTest {
+
+
+    @Test
+    @DisplayName("Testing otp code generator")
+    public void test_otp_code(){
+        Assertions.assertEquals(5, OtpCodeGenerator.generate().length());
+    }
+
 }
