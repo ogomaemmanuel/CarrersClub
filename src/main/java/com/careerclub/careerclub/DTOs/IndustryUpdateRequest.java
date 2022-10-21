03e1f5b41ec35b26954bfb90d6096833cc6ead95
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-10-21 11:56:25 +0300

    Added a dto for industry creation

--- src/main/java/com/careerclub/careerclub/DTOs/IndustryUpdateRequest.java
+++ src/main/java/com/careerclub/careerclub/DTOs/IndustryUpdateRequest.java
@@ -1,2 +1,13 @@
-package com.careerclub.careerclub.DTOs;public class IndustrUpdateRequest {
+package com.careerclub.careerclub.DTOs;
+
+public class IndustryUpdateRequest {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
