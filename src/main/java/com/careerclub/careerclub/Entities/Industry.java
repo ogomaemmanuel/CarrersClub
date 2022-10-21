8e541a3c5b585937d1e81029e5d44dcda3cc313c
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-10-21 11:56:52 +0300

    Added an industry entity

--- src/main/java/com/careerclub/careerclub/Entities/Industry.java
+++ src/main/java/com/careerclub/careerclub/Entities/Industry.java
@@ -1,2 +1,28 @@
-package com.careerclub.careerclub.Entities;public class Industry {
+package com.careerclub.careerclub.Entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "industries")
+public class Industry {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
