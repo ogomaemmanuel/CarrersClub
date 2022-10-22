6136d672465a464245cea87095c6d13778a07921
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-10-22 21:53:02 +0300

    Job creation DTO

--- src/main/java/com/careerclub/careerclub/DTOs/JobTypeRequest.java
+++ src/main/java/com/careerclub/careerclub/DTOs/JobTypeRequest.java
@@ -1,2 +1,13 @@
-package com.careerclub.careerclub.DTOs;public class JobTypeRequest {
+package com.careerclub.careerclub.DTOs;
+
+public class JobTypeRequest {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
