24734b81c791bf1e4ec7cc7566b300bec9bdf50f
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-10-22 21:54:06 +0300

    Enums for Jobtype

--- src/main/java/com/careerclub/careerclub/Utils/CheckEnums.java
+++ src/main/java/com/careerclub/careerclub/Utils/CheckEnums.java
@@ -1,2 +1,14 @@
-package com.careerclub.careerclub.Utils;public class CheckEnums {
-}
+//package com.careerclub.careerclub.Utils;
+//
+//import com.careerclub.careerclub.Enums.JobTypeEnums;
+//
+//public class CheckEnums {
+//    private JobTypeEnums jobTypeEnums;
+//    public boolean checkEnum(){
+//        switch (name){
+//            case jobTypeEnums.HYBRID:
+//
+//
+//        }
+//    }
+//}
