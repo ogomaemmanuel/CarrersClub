cdad9a6b90beb2b12e5f61ae2361040f8d8e4a21
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-10-22 06:04:24 +0300

    Created a location service

--- src/main/java/com/careerclub/careerclub/Service/LocationService.java
+++ src/main/java/com/careerclub/careerclub/Service/LocationService.java
@@ -1,7 +1,14 @@
 package com.careerclub.careerclub.Service;
 
+import com.careerclub.careerclub.Advice.RecordNotFoundException;
+import com.careerclub.careerclub.DTOs.LocationCreateRequest;
+import com.careerclub.careerclub.Entities.Location;
 import com.careerclub.careerclub.Repositories.LocationRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class LocationService {
@@ -10,4 +17,48 @@ public class LocationService {
     public LocationService(LocationRepository locationRepository) {
         this.locationRepository = locationRepository;
     }
+
+    public List<Location> getAllLocations(){
+        return locationRepository.findAll();
+    }
+
+    public Optional<Location> getLocationByName(String name){
+        var location = locationRepository.findByName(name);
+        if(location.isEmpty()){
+            throw new RecordNotFoundException("Location with the name "+name+" doesn't exist");
+        }
+        return location;
+    }
+
+    public Location createLocation(LocationCreateRequest locationCreateRequest){
+        var location = new Location();
+        location.setName(locationCreateRequest.getName());
+        locationRepository.save(location);
+        return  location;
+    }
+
+    public Optional<Location> updateLocation(String name,LocationCreateRequest locationCreateRequest){
+        var location = locationRepository.findByName(name);
+        location.ifPresentOrElse(l->{
+            l.setName(locationCreateRequest.getName());
+            locationRepository.save(l);
+        },()->{
+            throw new RecordNotFoundException("Location with name "+name+" doesn't exist.");
+        });
+        return location;
+    }
+
+    public HashMap<Object,Object> deleteLocation(String name){
+        var validate = new HashMap<>();
+        var location = locationRepository.findByName(name);
+        location.ifPresentOrElse(l->{
+            locationRepository.delete(l);
+            validate.put("message","Location deleted successfully.");
+        },()->{
+            throw new RecordNotFoundException("Location with the name "+name+" doesn't exist.");
+        });
+        return validate;
+    }
+
 }
+
