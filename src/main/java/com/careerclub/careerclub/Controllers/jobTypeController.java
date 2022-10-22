0cd4ca4467d1de017fdd0a091942f6cf99afce94
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-10-22 21:53:42 +0300

    Job type controller

--- src/main/java/com/careerclub/careerclub/Controllers/jobTypeController.java
+++ src/main/java/com/careerclub/careerclub/Controllers/jobTypeController.java
@@ -1,2 +1,25 @@
-package com.careerclub.careerclub.Controllers;public class jobTypeController {
+package com.careerclub.careerclub.Controllers;
+
+import com.careerclub.careerclub.DTOs.JobTypeRequest;
+import com.careerclub.careerclub.Entities.JobType;
+import com.careerclub.careerclub.Service.JobTypeService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/jobtype")
+public class jobTypeController {
+    private final JobTypeService jobTypeService;
+
+    public jobTypeController(JobTypeService jobTypeService) {
+        this.jobTypeService = jobTypeService;
+    }
+
+    @PostMapping
+    public ResponseEntity<JobType> postJobType(@RequestBody JobTypeRequest jobTypeRequest){
+        return ResponseEntity.ok(jobTypeService.createJobType(jobTypeRequest));
+    }
 }
