c8112a306808c428a211ae0e8dd6f57bf8cb2115
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-10-22 06:03:18 +0300

    Create location dto to handle location creation and update

--- src/main/java/com/careerclub/careerclub/DTOs/LocationCreateRequest.java
+++ src/main/java/com/careerclub/careerclub/DTOs/LocationCreateRequest.java
@@ -1,2 +1,16 @@
-package com.careerclub.careerclub.DTOs;public class LocationCreateRequest {
+package com.careerclub.careerclub.DTOs;
+
+import javax.validation.constraints.NotBlank;
+
+public class LocationCreateRequest {
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
