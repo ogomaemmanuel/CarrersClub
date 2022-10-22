9fd92ef98493102bf802198e8b1eadaca98cc260
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-10-22 06:03:40 +0300

    Added a location repository

--- src/main/java/com/careerclub/careerclub/Repositories/LocationRepository.java
+++ src/main/java/com/careerclub/careerclub/Repositories/LocationRepository.java
@@ -4,6 +4,10 @@ import com.careerclub.careerclub.Entities.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LocationRepository extends JpaRepository<Location,Long> {
+
+    Optional<Location> findByName(String name);
 }
