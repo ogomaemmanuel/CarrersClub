55dc87d0ec1cd0ab7595885dac48818afae88d26
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-10-22 10:46:02 +0300

    Added pagination to the fetch all company query

--- src/main/java/com/careerclub/careerclub/Repositories/CompanyRepository.java
+++ src/main/java/com/careerclub/careerclub/Repositories/CompanyRepository.java
@@ -1,9 +1,12 @@
 package com.careerclub.careerclub.Repositories;
 
 import com.careerclub.careerclub.Entities.Company;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
+    Page<Company> findAll(Pageable pageable);
 }
