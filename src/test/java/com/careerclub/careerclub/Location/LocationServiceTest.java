4f2d08d8845b26d084763165400297d2d32d1d99
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-10-23 10:46:14 +0300

    Removed test of all location

--- src/test/java/com/careerclub/careerclub/Location/LocationServiceTest.java
+++ src/test/java/com/careerclub/careerclub/Location/LocationServiceTest.java
@@ -1,2 +1,35 @@
-package com.careerclub.careerclub.Location;public class LocationServiceTest {
+package com.careerclub.careerclub.Location;
+
+import com.careerclub.careerclub.Entities.Location;
+import com.careerclub.careerclub.Repositories.LocationRepository;
+import com.careerclub.careerclub.Service.LocationService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class LocationServiceTest {
+
+    @InjectMocks
+    LocationService locationService;
+
+    @Mock
+    LocationRepository locationRepository;
+
+    @Test
+    @DisplayName("Testing all locations")
+    public void test_all_location(){
+        when(locationRepository.findAll()).thenReturn(new ArrayList<>());
+        locationService.getAllLocations();
+        verify(locationRepository).findAll();
+    }
+
 }
