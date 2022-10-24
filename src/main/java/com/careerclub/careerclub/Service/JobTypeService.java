9deb702cfd874690676de6967565b1892af7dc8a
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-10-24 12:12:18 +0300

    Enums now working

--- src/main/java/com/careerclub/careerclub/Service/JobTypeService.java
+++ src/main/java/com/careerclub/careerclub/Service/JobTypeService.java
@@ -8,6 +8,7 @@ import com.careerclub.careerclub.Repositories.JobTypeRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Locale;
 
 @Service
 public class JobTypeService {
@@ -24,12 +25,13 @@ public class JobTypeService {
     }
     public JobType createJobType(JobTypeRequest jobTypeRequest){
         JobType jobType = new JobType();
-        if(jobTypeRequest.getName().equals(jobTypeEnums.FULLTIME)){
+        checkingEnums(jobTypeRequest.getName());
+        do{
             jobType.setName(jobTypeRequest.getName());
             jobTypeRepository.save(jobType);
             return jobType;
-        }
-        throw new RecordNotFoundException("Job type is invalid");
+        } while(true);
+
     }
     public enum JobTypeEnums {
         FULLTIME,
@@ -37,6 +39,27 @@ public class JobTypeService {
         HYBRID,
         CONTRACT,
         INTERNSHIP
+    }
+    public boolean checkingEnums(String name){
+        var jobtype = jobTypeRepository.getByname(name.toUpperCase());
+        if (jobtype != null){
+            throw new RecordNotFoundException("Job type exists");
+        }else{
+            if(name.toUpperCase().equals(JobTypeEnums.FULLTIME.name())){
+                return true;
+            } else if (name.toUpperCase().equals(JobTypeEnums.REMOTE.name())) {
+                return true;
+            } else if (name.toUpperCase().equals(JobTypeEnums.CONTRACT.name())) {
+                return true;
+            } else if (name.toUpperCase().equals(JobTypeEnums.HYBRID.name())) {
+                return true;
+            } else if (name.toUpperCase().equals(JobTypeEnums.INTERNSHIP.name())) {
+                return true;
+            }else {
+                throw new RecordNotFoundException("Job type is invalid");
+            }
+        }
+
     }
 
 }
