da81db1d39f806da7c5158709ac061771676bac5
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-10-24 12:11:28 +0300

    Updated jobtype field in job creation

--- src/main/java/com/careerclub/careerclub/Seeders/DbSeeders.java
+++ src/main/java/com/careerclub/careerclub/Seeders/DbSeeders.java
@@ -1,13 +1,7 @@
 package com.careerclub.careerclub.Seeders;
 
-import com.careerclub.careerclub.Entities.Company;
-import com.careerclub.careerclub.Entities.Job;
-import com.careerclub.careerclub.Entities.Roles;
-import com.careerclub.careerclub.Entities.User;
-import com.careerclub.careerclub.Repositories.CompanyRepository;
-import com.careerclub.careerclub.Repositories.JobRepository;
-import com.careerclub.careerclub.Repositories.RolesRepository;
-import com.careerclub.careerclub.Repositories.UserRepository;
+import com.careerclub.careerclub.Entities.*;
+import com.careerclub.careerclub.Repositories.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -23,12 +17,14 @@ public class DbSeeders implements CommandLineRunner {
     private final RolesRepository rolesRepository;
     private final JobRepository jobRepository;
     private final CompanyRepository companyRepository;
+    private final JobTypeRepository jobTypeRepository;
 
-    public DbSeeders(UserRepository userRepository, RolesRepository rolesRepository, JobRepository jobRepository, CompanyRepository companyRepository) {
+    public DbSeeders(UserRepository userRepository, RolesRepository rolesRepository, JobRepository jobRepository, CompanyRepository companyRepository, JobTypeRepository jobTypeRepository) {
         this.userRepository = userRepository;
         this.rolesRepository = rolesRepository;
         this.jobRepository = jobRepository;
         this.companyRepository = companyRepository;
+        this.jobTypeRepository = jobTypeRepository;
     }
 
     @Override
@@ -36,6 +32,7 @@ public class DbSeeders implements CommandLineRunner {
         var userCount = userRepository.count();
         var roleCount = rolesRepository.count();
         var jobCount = jobRepository.count();
+        var jobTypeCount = jobRepository.count();
         var companyCount = companyRepository.count();
         if(roleCount==0){
             var rolesToAdd = new String[]{"admin", "member","hr"};
@@ -64,14 +61,20 @@ public class DbSeeders implements CommandLineRunner {
         }
         if(jobCount==0){
             var testCompany = companyRepository.findById(1L).get();
+            var testJobType = jobTypeRepository.findById(1L).get();
             var job = new Job();
             job.setTitle("DevOps Engineer");
             job.setDescription("Development and Deploying");
-            job.setJobType("Full-Time");
+            job.setJobType(testJobType);
             job.setDeadline("08/12/2022");
             job.setQualification("Docker & Kubernettes");
             job.setCompany(testCompany);
             jobRepository.save(job);
+        }
+        if(jobTypeCount == 0){
+            var jobType = new JobType();
+            jobType.setName("FULLTIME");
+            jobTypeRepository.save(jobType);
         }
     }
 }
