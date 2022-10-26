b625d9feb9fa39de189d104b1a6d78f90d2a6649
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-10-26 08:32:02 +0300

    Tested jobs retrieval by id endpoint

--- src/test/java/com/careerclub/careerclub/Jobs/JobsControllerTest.java
+++ src/test/java/com/careerclub/careerclub/Jobs/JobsControllerTest.java
@@ -1,2 +1,41 @@
-package com.careerclub.careerclub.Jobs;public class JobsControllerTest {
+package com.careerclub.careerclub.Jobs;
+
+
+import com.careerclub.careerclub.Controllers.JobsController;
+import com.careerclub.careerclub.Service.CustomUserDetailsService;
+import com.careerclub.careerclub.Service.JobsService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = {JobsController.class})
+@AutoConfigureMockMvc
+@ExtendWith(SpringExtension.class)
+public class JobsControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+    @MockBean
+    JobsService jobsService;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    CustomUserDetailsService customUserDetailsService;
+
+    @Test
+    @DisplayName("testing job retrieval by id")
+    public void test_job_id() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.get("/jobs/{id}",1)).andExpect(status().isNotFound());
+    }
 }
