03b3bb848213d10cb752737aed0548423dd88ec5
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-10-26 07:20:23 +0300

    Added an endpoint for updating the company

--- src/main/java/com/careerclub/careerclub/Controllers/CompanyController.java
+++ src/main/java/com/careerclub/careerclub/Controllers/CompanyController.java
@@ -1,4 +1,5 @@
 package com.careerclub.careerclub.Controllers;
+
 import com.careerclub.careerclub.DTOs.CompanyCreationRequest;
 import com.careerclub.careerclub.Entities.Company;
 import com.careerclub.careerclub.Service.CompanyService;
@@ -6,40 +7,44 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import  java.util.List;
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/companies")
 public class CompanyController {
-        private final CompanyService companyService;
-        public CompanyController(CompanyService companyService){
-            this.companyService = companyService;
-        }
+    private final CompanyService companyService;
 
-        @GetMapping
-        public ResponseEntity<?> getAllCompanies(Pageable pageable){
-            var companies = companyService.getAllCompanies(pageable);
-            return  ResponseEntity.ok(companies);
-        }
+    public CompanyController(CompanyService companyService) {
+        this.companyService = companyService;
+    }
 
-        @GetMapping("/{id}")
-        public ResponseEntity<Company> getCompanyById(@PathVariable Long id){
-            var company = companyService.getCompanyById(id);
-            return  ResponseEntity.of(company);
-        }
+    @GetMapping
+    public ResponseEntity<?> getAllCompanies(Pageable pageable) {
+        var companies = companyService.getAllCompanies(pageable);
+        return ResponseEntity.ok(companies);
+    }
 
-        @PostMapping
-        public ResponseEntity<Company> createCompany(@RequestBody CompanyCreationRequest newCompany){
-            var company = companyService.createCompany(newCompany);
-            return  ResponseEntity.ok(company);
-        }
+    @GetMapping("/{id}")
+    public ResponseEntity<Company> getCompanyById(@PathVariable Long id) {
+        var company = companyService.getCompanyById(id);
+        return ResponseEntity.of(company);
+    }
 
-    @DeleteMapping("/{id}")
-    public void deleteJob(@PathVariable Long id){
+    @PostMapping("/create")
+    public ResponseEntity<Company> createCompany(@RequestBody CompanyCreationRequest newCompany) {
+        var company = companyService.createCompany(newCompany);
+        return ResponseEntity.ok(company);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteJob(@PathVariable Long id) {
         companyService.companyToDelete(id);
     }
 
-//        @PutMapping
-//        public ResponseEntity<Company> updateCompany(){
-//        }
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> updateCompany(@PathVariable Long id, @Valid @RequestBody CompanyCreationRequest updateCompany) {
+        var company = companyService.updateCompany(id, updateCompany);
+        return ResponseEntity.of(company);
+    }
 }
