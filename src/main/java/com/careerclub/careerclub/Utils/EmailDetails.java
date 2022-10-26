a915c8019ece06aabfbf921b0cfd651f04d75e98
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-10-26 09:47:32 +0300

    .

--- src/main/java/com/careerclub/careerclub/Utils/EmailDetails.java
+++ src/main/java/com/careerclub/careerclub/Utils/EmailDetails.java
@@ -1,2 +1,32 @@
-package com.careerclub.careerclub.Utils;public class EmailDetails {
+package com.careerclub.careerclub.Utils;
+
+public class EmailDetails {
+    private String recipient;
+    private String msgBody;
+    private String subject;
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    public String getMsgBody() {
+        return msgBody;
+    }
+
+    public void setMsgBody(String msgBody) {
+        this.msgBody = msgBody;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
 }
