7d2986895b8c3ea0991882b77986f037c02e63e9
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-10-27 06:50:56 +0300

    Added a dto to help with data transfer on subscription to maillist

--- src/main/java/com/careerclub/careerclub/DTOs/MailListSubscribeRequest.java
+++ src/main/java/com/careerclub/careerclub/DTOs/MailListSubscribeRequest.java
@@ -1,2 +1,59 @@
-package com.careerclub.careerclub.DTOs;public class MailListSubscribeRequest {
+package com.careerclub.careerclub.DTOs;
+
+public class MailListSubscribeRequest {
+    private String alertName;
+    private String jobTypeName;
+    private String industryName;
+    private String location;
+    private Long userId;
+
+    private String companyName;
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getAlertName() {
+        return alertName;
+    }
+
+    public void setAlertName(String alertName) {
+        this.alertName = alertName;
+    }
+
+    public String getJobTypeName() {
+        return jobTypeName;
+    }
+
+    public void setJobTypeName(String jobTypeName) {
+        this.jobTypeName = jobTypeName;
+    }
+
+    public String getIndustryName() {
+        return industryName;
+    }
+
+    public void setIndustryName(String industryName) {
+        this.industryName = industryName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
 }
