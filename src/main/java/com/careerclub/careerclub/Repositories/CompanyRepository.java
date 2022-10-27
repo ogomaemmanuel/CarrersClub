49e6302f34331bc1cef5d9ad3013b56d5778927d
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-10-27 06:53:54 +0300

    Added a custom query to fetch a company by it's name

--- src/main/java/com/careerclub/careerclub/Repositories/CompanyRepository.java
+++ src/main/java/com/careerclub/careerclub/Repositories/CompanyRepository.java
@@ -6,7 +6,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
     Page<Company> findAll(Pageable pageable);
+
+    Optional<Company> findByName(String name);
 }
