1c179d045396b2fc2224f31ff2e8237405fca822
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-10-27 06:52:51 +0300

    Trial of events

--- src/main/java/com/careerclub/careerclub/Events/JobCreatedEvent.java
+++ src/main/java/com/careerclub/careerclub/Events/JobCreatedEvent.java
@@ -1,2 +1,16 @@
-package com.careerclub.careerclub.Events;public class JobCreatedEvent {
+package com.careerclub.careerclub.Events;
+
+import com.careerclub.careerclub.Entities.Job;
+
+public class JobCreatedEvent {
+
+    private Job job;
+
+    public JobCreatedEvent(Job job) {
+        this.job = job;
+    }
+
+    public Job getJob() {
+        return job;
+    }
 }
