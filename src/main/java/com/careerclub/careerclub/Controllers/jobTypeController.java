83938207a0216b54014c547037729b7cd9d8ded9
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-10-27 09:46:24 +0300

    Reviewwd

--- src/main/java/com/careerclub/careerclub/Controllers/jobTypeController.java
+++ src/main/java/com/careerclub/careerclub/Controllers/jobTypeController.java
@@ -4,10 +4,9 @@ import com.careerclub.careerclub.DTOs.JobTypeRequest;
 import com.careerclub.careerclub.Entities.JobType;
 import com.careerclub.careerclub.Service.JobTypeService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/jobtype")
@@ -18,6 +17,11 @@ public class jobTypeController {
         this.jobTypeService = jobTypeService;
     }
 
+    @GetMapping
+    public ResponseEntity<List<JobType>> getAllJobTypes(){
+        var types = jobTypeService.getAllJobs();
+        return ResponseEntity.ok(types);
+    }
     @PostMapping
     public ResponseEntity<JobType> postJobType(@RequestBody JobTypeRequest jobTypeRequest){
         return ResponseEntity.ok(jobTypeService.createJobType(jobTypeRequest));
