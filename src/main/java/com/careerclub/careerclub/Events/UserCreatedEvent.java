55b4afede4e1c774d752fc621157a5ad4b8cbb65
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-10-27 06:53:30 +0300

    Trial of events trigger

--- src/main/java/com/careerclub/careerclub/Events/UserCreatedEvent.java
+++ src/main/java/com/careerclub/careerclub/Events/UserCreatedEvent.java
@@ -1,2 +1,16 @@
-package com.careerclub.careerclub.Events;public class UserCreatedEvent {
+package com.careerclub.careerclub.Events;
+
+import com.careerclub.careerclub.Entities.User;
+
+public class UserCreatedEvent {
+    private User user;
+
+    public UserCreatedEvent(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
 }
