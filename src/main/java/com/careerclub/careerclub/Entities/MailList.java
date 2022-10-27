e2f82078e0b6536f583114629e7991518744224a
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-10-27 06:51:57 +0300

    Added a mail list entity

--- src/main/java/com/careerclub/careerclub/Entities/MailList.java
+++ src/main/java/com/careerclub/careerclub/Entities/MailList.java
@@ -1,2 +1,88 @@
-package com.careerclub.careerclub.Entities;public class MailList {
+package com.careerclub.careerclub.Entities;
+
+import javax.persistence.*;
+
+@Entity
+public class MailList {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String alertName;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "industry_id")
+    private Industry industry;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "jobtype_id")
+    private JobType jobType;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "location_id")
+    private Location location;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "company_id")
+    private Company company;
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAlertName() {
+        return alertName;
+    }
+
+    public void setAlertName(String alertName) {
+        this.alertName = alertName;
+    }
+
+    public Industry getIndustry() {
+        return industry;
+    }
+
+    public void setIndustry(Industry industry) {
+        this.industry = industry;
+    }
+
+    public JobType getJobType() {
+        return jobType;
+    }
+
+    public void setJobType(JobType jobType) {
+        this.jobType = jobType;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
