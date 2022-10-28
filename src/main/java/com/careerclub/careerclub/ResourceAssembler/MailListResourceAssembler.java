59b4e5725b2ee3ce2293122b7182ccf8893466ba
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-10-28 10:15:17 +0300

    Extended the maillist enity with representation model to handle links creations

--- src/main/java/com/careerclub/careerclub/ResourceAssembler/MailListResourceAssembler.java
+++ src/main/java/com/careerclub/careerclub/ResourceAssembler/MailListResourceAssembler.java
@@ -1,2 +1,21 @@
-package com.careerclub.careerclub.ResourceAssembler;public class MailListResourceAssembler {
+package com.careerclub.careerclub.ResourceAssembler;
+
+import com.careerclub.careerclub.Controllers.MailListController;
+import com.careerclub.careerclub.Entities.MailList;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Component
+public class MailListResourceAssembler implements RepresentationModelAssembler<MailList, EntityModel<MailList>> {
+
+    @Override
+    public EntityModel<MailList> toModel(MailList entity) {
+        EntityModel<MailList> model = EntityModel.of(entity);
+        model.add(linkTo(methodOn(MailListController.class).unsubscribeFormList(entity.getId(), entity.getUser().getId())).withSelfRel());
+        return model;
+    }
 }
