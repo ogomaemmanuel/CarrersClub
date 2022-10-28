bb3fbd740270b4a05ab3289929a2e803a4ad4937
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-10-28 06:55:43 +0300

    Added a service to handle job retieval report

--- src/main/java/com/careerclub/careerclub/Report/ReportsService.java
+++ src/main/java/com/careerclub/careerclub/Report/ReportsService.java
@@ -1,2 +1,24 @@
-package com.careerclub.careerclub.Report;public interface ReportsRepository {
+package com.careerclub.careerclub.Report;
+
+
+import com.careerclub.careerclub.Repositories.JobRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Service
+
+public class ReportsService {
+    private final JobRepository jobRepository;
+
+    public ReportsService(JobRepository jobRepository) {
+        this.jobRepository = jobRepository;
+    }
+
+
+    public List<JobsByLocationReports> getJobsByLocation() {
+        return this.jobRepository.getJobsByLocations();
+    }
 }
