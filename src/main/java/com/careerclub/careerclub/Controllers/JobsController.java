376502b6c9934e34c4b1a4e84b803aeda62a62d6
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-10-28 10:13:44 +0300

    Added a resource model for job creation

--- src/main/java/com/careerclub/careerclub/Controllers/JobsController.java
+++ src/main/java/com/careerclub/careerclub/Controllers/JobsController.java
@@ -4,24 +4,24 @@ import com.careerclub.careerclub.DTOs.JobPostingRequest;
 import com.careerclub.careerclub.DTOs.JobUpdatingRequest;
 import com.careerclub.careerclub.DTOs.JobsFilter;
 import com.careerclub.careerclub.Entities.Job;
+import com.careerclub.careerclub.ResourceAssembler.JobResourceAssembler;
 import com.careerclub.careerclub.Service.JobsService;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Tag(name = "Jobs controller")
 @RestController
 @RequestMapping(value = "/jobs")
 public class JobsController {
     private final JobsService jobsService;
+    private final JobResourceAssembler jobResourceAssembler;
 
-    public JobsController(JobsService jobsService) {
+    public JobsController(JobsService jobsService, JobResourceAssembler jobResourceAssembler) {
         this.jobsService = jobsService;
+        this.jobResourceAssembler = jobResourceAssembler;
     }
 
     @GetMapping
@@ -34,8 +34,13 @@ public class JobsController {
     }
 
     @PostMapping
-    public ResponseEntity<Job> postNewJob(@RequestBody JobPostingRequest jobPostingRequest){
-        return ResponseEntity.ok(jobsService.postJob(jobPostingRequest));
+    public ResponseEntity<?> postNewJob(@RequestBody JobPostingRequest jobPostingRequest){
+        var job = jobsService.postJob(jobPostingRequest);
+
+        //Add a get link
+        var jobWithLink = jobResourceAssembler.toModel(job);
+
+        return ResponseEntity.ok(jobWithLink);
     }
     @DeleteMapping("/delete/{id}")
     public void deleteJob(@PathVariable Long id){
