24c4132555ec33924ae1705d807c43a32c741bc7
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-10-28 06:56:01 +0300

    Created a custom query

--- src/main/java/com/careerclub/careerclub/Repositories/UserRepository.java
+++ src/main/java/com/careerclub/careerclub/Repositories/UserRepository.java
@@ -6,10 +6,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 
     Page<User> findAll(Pageable pageable);
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
     User findByEmail(String email);
 }
