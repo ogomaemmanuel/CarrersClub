bbf815284d3f9dea30b2d76b6c38e6fc4c3fd652
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-11-07 14:43:00 +0300

    Added dummy mail pass and username

--- src/main/java/com/careerclub/careerclub/Repositories/ApplicationRepository.java
+++ src/main/java/com/careerclub/careerclub/Repositories/ApplicationRepository.java
@@ -4,6 +4,9 @@ import com.careerclub.careerclub.Entities.Application;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
+    Optional<Application> findByJobIdAndUserId(Long jobId,Long usedId);
 }
